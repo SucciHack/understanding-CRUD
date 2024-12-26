005d41696c41912a5d81ef2394addf28052d76af
Kaweesi Samuel <158592803+SucciHack@users.noreply.github.com>
2024-12-27 00:24:06 +0300

    updated functionalities

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const search = document.getElementById('bx-search');
 const closeBtn = document.querySelector('.closeBtn');
 const slideNav = document.querySelector('.slideNav');
 const slideNavShow = document.getElementById('slideNavShow');
+const productsDetails = document.querySelector('.productsDetails');
+
 slideNavShow.addEventListener('click', () => {
     slideNav.classList.add('active');
 })
@@ -50,7 +52,7 @@ function renderData(data) {
     products.innerHTML = '';
     data.forEach(item => {
         productTemplate = `
-        <div class="product">
+        <div class="product" data-id="${item.id}">
             <img src="${item.image}" alt="${item.name}">
             <p>${item.name}</p>
             <p>$ 49.75</p>
@@ -58,6 +60,14 @@ function renderData(data) {
         `;
         products.insertAdjacentHTML('beforeend', productTemplate);  
         
+    });
+    document.querySelectorAll('.product').forEach(product => {
+        product.addEventListener('click', () => {
+            const productId = product.getAttribute('data-id');
+            const productDetails = allProducts.find(item => item.id == productId);
+            localStorage.setItem('productDetails', JSON.stringify(productDetails));
+            window.location.href = 'productDetails.html';
+        });
     });
 }
 
@@ -94,3 +104,33 @@ cosmetics.addEventListener("click", ()=>{
     const filteredCosmetics = filterCosmetics()
     renderData(filteredCosmetics);
 })
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const productDetails = JSON.parse(localStorage.getItem('productDetails'));
+    if (productDetails) {
+        const productDetailsContainer = document.getElementById('productDetails');
+        const productDetailTemplate = `
+        <div class="productDetails" data-id="${productDetails.id}">
+            <img src="${productDetails.image}" alt="${productDetails.name}">
+            <div class="productDetailsText">
+                <h1>${productDetails.name}</h1>
+                <p>${productDetails.description}</p>
+                <p>$ 49.75</p>
+            </div>
+        </div>
+        `;
+        productDetailsContainer.insertAdjacentHTML('beforeend', productDetailTemplate);
+    } else {
+        productDetailsContainer.innerHTML = '<p>No product details found.</p>';
+    }
+});
+
+const searchContainer = document.querySelector('.search');
+const searchInput = document.getElementById('searchInput');
+
+searchInput.addEventListener('input', (e) => {
+    const searchTerm = e.target.value.toLowerCase();
+    const  filteredProducts = allProducts.filter(item => item.name.toLowerCase().includes(searchTerm)|| item.description.toLowerCase().includes(searchTerm));
+    renderData(filteredProducts);
+});
